f900517ab4b73d21cd1a65d616144d3c265febf9
Fatih <fatihselimkeskin@gmail.com>
2026-04-26 17:21:41 +0300

    chore(backend): move properties and sales modules to backend/core/

--- modules/crm/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-crm_bp = Blueprint('crm', __name__)
-
-from . import lead_routes
-from . import pipeline_routes
-from . import appointment_routes
-from . import contact_routes
--- modules/finance/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-finance_bp = Blueprint('finance', __name__)
-
-from . import routes
-from . import expense_routes
-from . import tax_routes
-from . import contract_routes
