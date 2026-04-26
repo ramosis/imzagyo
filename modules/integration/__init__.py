0b6c8a99ac88fe0f6c42b7730fb6a5ff9b8649b1
Fatih <fatihselimkeskin@gmail.com>
2026-04-26 17:45:38 +0300

    chore(backend): move remaining modules (core, integration, legal, mobile)

--- modules/integration/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-integration_bp = Blueprint('integration', __name__)
-
-from modules.integration import routes
--- modules/legal/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-legal_bp = Blueprint('legal', __name__)
-
-from . import routes
-from . import party_routes
-from . import inspection_routes
